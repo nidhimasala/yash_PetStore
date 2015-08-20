599ae07d1139587d36b1172c6dba6fc0e66177a4
quanghtran <quanggre@gmail.com>
2015-08-20 14:44:22 -0500

    Importing Nidi masala

---
+++ src/main/java/com/yash/project/com/yash/project/petstore/App.java
@@ -0,0 +1,13 @@
+package com.yash.project.com.yash.project.petstore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
